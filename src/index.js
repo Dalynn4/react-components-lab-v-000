70a24e10601cb67a51b169a29b80d2643c3690c0
Randall Robinson <keith_adams007@yahoo.com>
2018-07-23 19:12:43 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -39,7 +39,15 @@ export class InFrontOfYou extends Component {
 export class ButcherShop extends Component {
   render() {
     return (
-      {BUTCHER_PRODUCTS}
+      <div class="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    <li>Tenderloin</li>
+    <li>Short ribs</li>
+    <li>Beef shin</li>
+    <li>Ribeye</li>
+  </ul>
+</div>
     )
   }
 }
